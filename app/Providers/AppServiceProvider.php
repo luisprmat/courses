f52aa76654a2766a66736cd3a542ee481cddd6bc
Luis Antonio Parrado <luisprmat@gmail.com>
2021-06-02 01:00:58 -0500

    Add CRUD for course's lessons wih regex validations - observers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Lesson;
+use App\Observers\LessonObserver;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
@@ -25,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Lesson::observe(LessonObserver::class);
+
         Blade::if('routeIs', function($route, $params = []) {
             return Request::url() == route($route, $params);
         });
