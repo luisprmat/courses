827d08820696eded674e9b1b50a0ffcc93dfd59d
Luis Antonio Parrado <luisprmat@gmail.com>
2021-06-04 20:14:30 -0500

    Send mails through queues

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -36,7 +36,7 @@ class CourseController extends Controller
         $course->save();
 
         $mail = new ApprovedCourse($course);
-        Mail::to($course->teacher->email)->send($mail);
+        Mail::to($course->teacher->email)->queue($mail);
 
         return redirect()->route('admin.courses.index')->with('info', 'El curso se publicó con éxito');
     }
